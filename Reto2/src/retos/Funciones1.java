b5a3aa6e45c736eda3ea5bd93f9825a45660a2ce
valeria <vebriferr@gmail.com>
2024-10-31 09:45:01 +0100

    a

--- Reto2/src/retos/Funciones1.java
+++ Reto2/src/retos/Funciones1.java
@@ -6,55 +6,53 @@ import util.Funciones;
 
 public class Funciones1 {
 
-	public static void menu2(int numero, Scanner sc) {
-	do {
-		System.out.println("Selecciona la conversión de longitud");
-		System.out.println("1. Millas a kilometros");
-		System.out.println("2. Kilometros a millas");
-		System.out.println("0. Salir");
-		
-		Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc)
-		switch (numero) {
-		  case 1:
-			  millasaKilometros(numero);
-			  break;
-		  case 2:
-			  kilosaLibras(numero);
-		    break;
-		  case 0:
-			  System.out.println("Has salido");
-		    break;
-		
-		}
-	}while(numero!=0);
-	
-	
-	}
-
-	public static void menu1(int numero, Scanner sc) {
+	public static void menuLongitud(Scanner sc) {
+		int opcion = 0;
 		do {
 			System.out.println("Selecciona la conversión de longitud");
 			System.out.println("1. Millas a kilometros");
 			System.out.println("2. Kilometros a millas");
 			System.out.println("0. Salir");
-			
-			//Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc)
-			switch (numero) {
-			  case 1:
-				  librasaKilogramos(numero);
-				  break;
-			  case 2:
-				  kilometrosaMillas(numero);
-			    break;
-			  case 0:
-				  System.out.println("Has salido");
-			    break;
-			
+
+			opcion = Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc);
+			switch (opcion) {
+			case 1:
+				double kilometros = millasaKilometros(Funciones.dimeDouble("dime las millas", sc));
+				System.out.println("Los kilometros son " + kilometros);
+				break;
+			case 2:
+				double millas = kilometrosaMillas(Funciones.dimeDouble("dime los kilometros", sc));
+				System.out.println("Las millas son " + millas);
+				break;
+
 			}
-		}while(numero!=0);
-		
-		
-		}}
+		} while (opcion != 0);
+
+	}
+
+	public static void menuPeso(Scanner sc) {
+		int opcion = 0;
+		do {
+			System.out.println("Selecciona la conversión de longitud");
+			System.out.println("1. Libras a kilogramos");
+			System.out.println("2. Kilogramos a libras");
+			System.out.println("0. Salir");
+
+			opcion = Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc);
+			switch (opcion) {
+			case 1:
+				double kilogramos = librasaKilogramos(Funciones.dimeDouble("dime las libras", sc));
+				System.out.println("Los kilogramos son " + kilogramos);
+				break;
+			case 2:
+				double libras = kilosaLibras(Funciones.dimeDouble("dime los kilogramos", sc));
+				System.out.println("Las libras son " + libras);
+				break;
+
+			}
+		} while (opcion != 0);
+
+	}
 
 	// Millas a km
 	public static double millasaKilometros(double millas) { // si pulsa uno sale esto
