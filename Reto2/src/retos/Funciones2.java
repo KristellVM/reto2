85102fdc71ab89f30ecfb55489f3f89481b1bd4b
KristellVM <krisvm31@gmail.com>
2024-10-31 08:39:31 +0100

    creada funcion litros a galones

--- Reto2/src/retos/Funciones2.java
+++ Reto2/src/retos/Funciones2.java
@@ -9,4 +9,8 @@ public class Funciones2 {
 	public static double fahrenheitACelsius(double num) {
 		return (num-32)*5/9;
 	}
+	
+	public static double litrosAGalones(double num) {
+		return num*0.264172;
+	}
 }
