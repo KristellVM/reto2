9b6accbb4236b398210c88002819455c836b0cc1
valeria <vebriferr@gmail.com>
2024-10-31 09:27:45 +0100

    Merge branch 'main' of https://github.com/KristellVM/reto2.git into main

--- Reto2/src/retos/Funciones1.java
+++ Reto2/src/retos/Funciones1.java
@@ -1,31 +1,90 @@
 package retos;
 
+import java.util.Scanner;
+
+import util.Funciones;
+
 public class Funciones1 {
 
-	public static void main(String[] args) {}
-	
-	//Millas a km
-	public static double kilometros(double millas) {
-		return  millas*1.69;
+	public static void menu2(int numero, Scanner sc) {
+	do {
+		System.out.println("Selecciona la conversión de longitud");
+		System.out.println("1. Millas a kilometros");
+		System.out.println("2. Kilometros a millas");
+		System.out.println("0. Salir");
 		
-	}
-	
-	//Km a millas
-	public static double millas(double kilometros) {
-		return  kilometros/1.69;
+		Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc)
+		switch (numero) {
+		  case 1:
+			  millasaKilometros(numero);
+			  break;
+		  case 2:
+			  kilosaLibras(numero);
+		    break;
+		  case 0:
+			  System.out.println("Has salido");
+		    break;
 		
+		}
+	}while(numero!=0);
+	
+	
 	}
-	
-	
-	//libras a kilos
-	public static double kilogramos(double libras) {
-		return  libras/2.205;
-		
-	}
-	
-	
 
-	//kilos a libras
-	public static double libras(double kilogramos) {
-		return  kilogramos*2.205;
-}}
+	
+	public static void menu1(int numero, Scanner sc) {
+		do {
+			System.out.println("Selecciona la conversión de longitud");
+			System.out.println("1. Millas a kilometros");
+			System.out.println("2. Kilometros a millas");
+			System.out.println("0. Salir");
+			
+			//Funciones.dimeEntero("Elige opcion:\n1-opcion1\n2-opcion2\0-Salir", sc)
+			switch (numero) {
+			  case 1:
+				  librasaKilogramos(numero);
+				  break;
+			  case 2:
+				  kilometrosaMillas(numero);
+			    break;
+			  case 0:
+				  System.out.println("Has salido");
+			    break;
+			
+			}
+		}while(numero!=0);
+		
+		
+		}}
+
+
+	
+	
+	
+	// Millas a km
+	public static double millasaKilometros(double millas) { //si pulsa uno sale esto
+		return millas * 1.69;
+
+	}
+
+	// Km a millas
+	public static double kilometrosaMillas(double kilometros) {
+		return kilometros / 1.69;
+
+	}
+
+	// libras a kilos
+	public static double librasaKilogramos(double libras) {
+		return libras / 2.205;
+
+	}
+
+	// kilos a libras
+	public static double kilosaLibras(double kilogramos) {
+		return kilogramos * 2.205;
+	}
+}
+
+
+
+
