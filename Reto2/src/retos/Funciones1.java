616e60d09c9ac3b51cd20641489f2f18cabc2945
valeria <vebriferr@gmail.com>
2024-10-31 08:41:24 +0100

    creadas funciones de peso y longitud

--- Reto2/src/retos/Funciones1.java
+++ Reto2/src/retos/Funciones1.java
@@ -2,4 +2,30 @@ package retos;
 
 public class Funciones1 {
 
-}
+	public static void main(String[] args) {}
+	
+	//Millas a km
+	public static double kilometros(double millas) {
+		return  millas*1.69;
+		
+	}
+	
+	//Km a millas
+	public static double millas(double kilometros) {
+		return  kilometros/1.69;
+		
+	}
+	
+	
+	//libras a kilos
+	public static double kilogramos(double libras) {
+		return  libras/2.205;
+		
+	}
+	
+	
+
+	//kilos a libras
+	public static double libras(double kilogramos) {
+		return  kilogramos*2.205;
+}}
